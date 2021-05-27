8154bd6e39642cd3f0f057a1907680925f1f53c2
mo3tafa <mostafafatehinia@gmail.com>
2021-05-27 17:22:50 +0430

    table`s content get from api

--- static/js/select_box_countries.js
+++ static/js/select_box_countries.js
@@ -1,4 +1,6 @@
 let select = document.getElementById('select-box');
+var myChart;
+
 let data = fetch('https://restcountries.eu/rest/v2/all').then(res => res.json()).then(data => {
 
     let html;
@@ -18,4 +20,31 @@ function parenthesesRemover(input, index) {
 
     return input[index].name.split('(')[0]
 
+}
+
+select.onchange = function(event) {
+
+    let country = event.target.value;
+    let date = new Date().toISOString().substr(0, 10);
+    let url = `https://api.covid19tracking.narrativa.com/api/${date}/country/${country}`
+
+    fetch(url).then(res => res.json()).then(data => {
+        let infoPerCountry = data.dates[date].countries[country]
+
+        tableBodyCreate(infoPerCountry)
+
+    })
+}
+
+function tableBodyCreate(input) {
+    let tbody = document.getElementById('table-body');
+    let rate = ((input.today_deaths - input.yesterday_deaths) / input.yesterday_deaths) * 100
+        // console.log(tbody.innerHTML)
+    let html = '<tr>';
+    html += `<td><i class="flag flag-${ String(input.name).toLowerCase()}" title=${ input.name }></i></td>`;
+    html += `<td> ${ input.today_confirmed } </td>`;
+    html += `<td> ${ input.today_recovered } </td>`;
+    html += `<td> ${ input.today_deaths } </td>`;
+    // console.log(html)
+    tbody.innerHTML = html;
 }
